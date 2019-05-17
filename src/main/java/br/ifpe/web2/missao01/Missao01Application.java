04e0c9ad23a067b29485bd4cdfea2adf62682cca
Emerson <emersonolvrsouza@gmail.com>
2019-05-17 00:08:18 +0000

    Missão 01

---
+++ src/main/java/br/ifpe/web2/missao01/Missao01Application.java
@@ -0,0 +1,13 @@
+package br.ifpe.web2.missao01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Missao01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Missao01Application.class, args);
+	}
+
+}
